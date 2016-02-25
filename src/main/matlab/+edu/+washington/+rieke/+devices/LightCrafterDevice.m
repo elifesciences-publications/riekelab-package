8c2a5074ca7acea5a2ed7a0f90505dfd9aaa839e
Mark Cafaro <mark.cafaro@gmail.com>
2016-02-24 19:04:52 -0800

    LightCrafterDevice just needs to set compositor now

--- src/main/matlab/+edu/+washington/+rieke/+devices/LightCrafterDevice.m
+++ src/main/matlab/+edu/+washington/+rieke/+devices/LightCrafterDevice.m
@@ -1,6 +1,7 @@
-classdef LightCrafterDevice < io.github.stage_vss.devices.StageDevice
+classdef LightCrafterDevice < symphonyui.core.Device
     
     properties (Access = private)
+        stageClient
         lightCrafter
         patternRatesToAttributes
     end
@@ -10,35 +11,101 @@ classdef LightCrafterDevice < io.github.stage_vss.devices.StageDevice
         function obj = LightCrafterDevice()
             host = 'localhost';
             port = 5678;
-            obj@io.github.stage_vss.devices.StageDevice(host, port, 'name', ['LightCrafter.Stage@' host]);
             
-            refreshRate = obj.stageClient.getMonitorRefreshRate();
+            cobj = Symphony.Core.UnitConvertingExternalDevice(['LightCrafter.Stage@' host], 'Unspecified', Symphony.Core.Measurement(0, symphonyui.core.Measurement.UNITLESS));
+            obj@symphonyui.core.Device(cobj);
+            obj.cobj.MeasurementConversionTarget = symphonyui.core.Measurement.UNITLESS;
             
-            obj.lightCrafter = LightCrafter4500(refreshRate);
+            obj.stageClient = stage.core.network.StageClient();
+            obj.stageClient.connect(host, port);
+            obj.stageClient.setMonitorGamma(1);
+            
+            trueCanvasSize = obj.stageClient.getCanvasSize();
+            canvasSize = [trueCanvasSize(1) * 2, trueCanvasSize(2)];
+            
+            obj.stageClient.setCanvasProjectionIdentity();
+            obj.stageClient.setCanvasProjectionOrthographic(0, canvasSize(1), 0, canvasSize(2));
+            
+            obj.lightCrafter = LightCrafter4500(obj.stageClient.getMonitorRefreshRate());
             obj.lightCrafter.connect();
             obj.lightCrafter.setMode('pattern');
+            obj.lightCrafter.setLedEnables(true, false, false, false);
+            [auto, red, green, blue] = obj.lightCrafter.getLedEnables();
             
-            m = containers.Map('KeyType', 'double', 'ValueType', 'any');
-            m(1 * refreshRate)  = {8, 'white', 1};
-            m(2 * refreshRate)  = {8, 'white', 2};
-            m(4 * refreshRate)  = {6, 'white', 4};
-            m(6 * refreshRate)  = {4, 'white', 6};
-            m(8 * refreshRate)  = {3, 'white', 8};
-            m(12 * refreshRate) = {2, 'white', 12};
-            m(24 * refreshRate) = {1, 'white', 24};
-            obj.patternRatesToAttributes = m;
+            refreshRate = obj.stageClient.getMonitorRefreshRate();
+            obj.patternRatesToAttributes = containers.Map('KeyType', 'double', 'ValueType', 'any');
+            obj.patternRatesToAttributes(1 * refreshRate)  = {8, 'white', 1};
+            obj.patternRatesToAttributes(2 * refreshRate)  = {8, 'white', 2};
+            obj.patternRatesToAttributes(4 * refreshRate)  = {6, 'white', 4};
+            obj.patternRatesToAttributes(6 * refreshRate)  = {4, 'white', 6};
+            obj.patternRatesToAttributes(8 * refreshRate)  = {3, 'white', 8};
+            obj.patternRatesToAttributes(12 * refreshRate) = {2, 'white', 12};
+            obj.patternRatesToAttributes(24 * refreshRate) = {1, 'white', 24};
             
-            obj.addConfigurationSetting('lightCrafterLedEnables', true(1, 4), 'isReadOnly', true);
-            obj.addConfigurationSetting('lightCrafterPatternRate', 0, 'isReadOnly', true);
+            attributes = obj.patternRatesToAttributes(refreshRate);
+            obj.lightCrafter.setPatternAttributes(attributes{:});
             
-            obj.setLedEnables(true, true, true, true);
-            obj.setPatternRate(refreshRate);
+            renderer = stage.builtin.renderers.PatternRenderer(attributes{3}, attributes{1});
+            obj.stageClient.setCanvasRenderer(renderer);
+            
+            obj.addConfigurationSetting('canvasSize', canvasSize, 'isReadOnly', true);
+            obj.addConfigurationSetting('trueCanvasSize', trueCanvasSize, 'isReadOnly', true);
+            obj.addConfigurationSetting('monitorRefreshRate', refreshRate, 'isReadOnly', true);
+            obj.addConfigurationSetting('lightCrafterLedEnables',  [auto, red, green, blue], 'isReadOnly', true);
+            obj.addConfigurationSetting('lightCrafterPatternRate', obj.lightCrafter.currentPatternRate(), 'isReadOnly', true);
         end
         
         function close(obj)
-            close@io.github.stage_vss.devices.StageDevice(obj);
-            
+            %obj.stageClient.resetCanvasProjection();
+            %obj.stageClient.resetCanvasRenderer();
+            obj.stageClient.disconnect();
             obj.lightCrafter.disconnect();
+        end
+        
+        function s = getCanvasSize(obj)
+            s = obj.getConfigurationSetting('canvasSize');
+        end
+        
+        function s = getTrueCanvasSize(obj)
+            s = obj.getConfigurationSetting('trueCanvasSize');
+        end
+        
+        function r = getMonitorRefreshRate(obj)
+            r = obj.getConfigurationSetting('monitorRefreshRate');
+        end
+        
+        function play(obj, presentation, prerender)
+            if nargin < 3
+                prerender = false;
+            end
+            
+            canvasSize = obj.getCanvasSize();
+            
+            background = stage.builtin.stimuli.Rectangle();
+            background.size = canvasSize;
+            background.position = canvasSize/2;
+            background.color = presentation.backgroundColor;
+            presentation.setBackgroundColor(0);
+            presentation.insertStimulus(1, background);
+            
+            tracker = stage.builtin.stimuli.FrameTracker();
+            tracker.size = [canvasSize(1) * 1/8, canvasSize(2)];
+            tracker.position = [canvasSize(1) - (canvasSize(1)/16), canvasSize(2)/2];
+            presentation.addStimulus(tracker);
+            
+            obj.stageClient.play(presentation, prerender);
+        end
+        
+        function replay(obj)
+            obj.stageClient.replay();
+        end
+        
+        function i = getPlayInfo(obj)
+            i = obj.stageClient.getPlayInfo();
+        end
+        
+        function clearMemory(obj)
+           obj.stageClient.clearMemory();
         end
         
         function setLedEnables(obj, auto, red, green, blue)
@@ -62,6 +129,9 @@ classdef LightCrafterDevice < io.github.stage_vss.devices.StageDevice
             attributes = obj.patternRatesToAttributes(rate);
             obj.lightCrafter.setPatternAttributes(attributes{:});
             obj.setReadOnlyConfigurationSetting('lightCrafterPatternRate', obj.lightCrafter.currentPatternRate());
+            
+            renderer = stage.builtin.renderers.PatternRenderer(attributes{3}, attributes{1});
+            obj.stageClient.setCanvasRenderer(renderer);
         end
         
         function r = getPatternRate(obj)
