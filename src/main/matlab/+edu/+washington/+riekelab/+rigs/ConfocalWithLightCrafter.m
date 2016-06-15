8a271a82a5a955403ed7c39b6bb050a3abbf6972
Mark Cafaro <mark.cafaro@gmail.com>
2016-06-15 11:19:33 -0700

    Fixed lightcrafter rig imports

--- src/main/matlab/+edu/+washington/+riekelab/+rigs/ConfocalWithLightCrafter.m
+++ src/main/matlab/+edu/+washington/+riekelab/+rigs/ConfocalWithLightCrafter.m
@@ -3,9 +3,12 @@ classdef ConfocalWithLightCrafter < edu.washington.riekelab.rigs.Confocal
     methods
         
         function obj = ConfocalWithLightCrafter()
+            import symphonyui.builtin.devices.*;
+            import edu.washington.*;
+            
             daq = obj.daqController;
             
-            lightCrafter = edu.washington.riekelab.devices.LightCrafterDevice('micronsPerPixel', 1.3);
+            lightCrafter = riekelab.devices.LightCrafterDevice('micronsPerPixel', 1.3);
             lightCrafter.bindStream(daq.getStream('DIGITAL_OUT.1'));
             daq.getStream('DIGITAL_OUT.1').setBitPosition(lightCrafter, 15);
             obj.addDevice(lightCrafter);
--- src/main/matlab/+edu/+washington/+riekelab/+rigs/OldSliceWithLightCrafter.m
+++ src/main/matlab/+edu/+washington/+riekelab/+rigs/OldSliceWithLightCrafter.m
@@ -3,9 +3,12 @@ classdef OldSliceWithLightCrafter < edu.washington.riekelab.rigs.OldSlice
     methods
         
         function obj = OldSliceWithLightCrafter()
+            import symphonyui.builtin.devices.*;
+            import edu.washington.*;
+            
             daq = obj.daqController;
             
-            lightCrafter = edu.washington.riekelab.devices.LightCrafterDevice('micronsPerPixel', 1.6);
+            lightCrafter = riekelab.devices.LightCrafterDevice('micronsPerPixel', 1.6);
             lightCrafter.bindStream(daq.getStream('DIGITAL_OUT.1'));
             daq.getStream('DIGITAL_OUT.1').setBitPosition(lightCrafter, 15);
             obj.addDevice(lightCrafter);
