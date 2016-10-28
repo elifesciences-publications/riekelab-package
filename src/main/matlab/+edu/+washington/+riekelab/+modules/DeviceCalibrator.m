7dd762fec60bf49042398c94ee57287a25dfd514
Mark Cafaro <mark.cafaro@gmail.com>
2016-10-28 14:47:31 -0700

    A lot of the calib gui is hooked up

--- src/main/matlab/+edu/+washington/+riekelab/+modules/DeviceCalibrator.m
+++ src/main/matlab/+edu/+washington/+riekelab/+modules/DeviceCalibrator.m
@@ -1,6 +1,12 @@
 classdef DeviceCalibrator < symphonyui.ui.Module
     
     properties (Access = private)
+        leds
+        stage
+        calibrations
+    end
+    
+    properties (Access = private)
         wizardCardPanel
         instructionsCard
         calibrationCard
@@ -195,23 +201,181 @@ classdef DeviceCalibrator < symphonyui.ui.Module
     methods (Access = protected)
         
         function willGo(obj)
+            obj.leds = obj.configurationService.getDevices('LED');
+            stages = obj.configurationService.getDevices('Stage');
+            if isempty(stages)
+                obj.stage = [];
+            else
+                obj.stage = stages{1};
+            end
+            
+            obj.calibrations = containers.Map();
+            
+            obj.populateDeviceList();
+            
             obj.updateStateOfControls();
+        end
+        
+        function onViewSelectedClose(obj, ~, ~)
+            obj.close();
         end
         
     end
     
     methods (Access = private)
         
+        function populateDeviceList(obj)
+            devices = containers.Map();
+            gains = {};
+            for i = 1:numel(obj.leds)
+                led = obj.leds{i};
+                
+                desc = led.getConfigurationSettingDescriptors().findByName('gain');
+                if isempty(desc)
+                    if devices.isKey('none')
+                        devices('none') = [devices('none') {led}];
+                    else
+                        devices('none') = {led};
+                    end
+                    continue;
+                end
+                
+                availableGains = desc.type.domain;
+                for k = 1:numel(availableGains)
+                    gain = availableGains{k};
+                    if isempty(gain)
+                        continue;
+                    end
+                    
+                    if devices.isKey(gain)
+                        devices(gain) = [devices(gain) {led}];
+                    else
+                        devices(gain) = {led};
+                        gains{end + 1} = gain; %#ok<AGROW>
+                    end
+                end
+            end
+            
+            keys = gains;
+            if devices.isKey('none')
+                keys{end + 1} = 'none';
+            end
+            
+            names = {};
+            values = {};
+            for i = 1:numel(keys)
+                key = keys{i};
+                devs = devices(key);
+                for k = 1:numel(devs)
+                    d = devs{k};
+                    n = d.name;
+                    if ~strcmp(key, 'none')
+                        n = [n ' - ' key]; %#ok<AGROW>
+                    end
+                    names{end + 1} = n; %#ok<AGROW>
+                    values{end + 1} = struct('device', d, 'gain', key); %#ok<AGROW>
+                end
+            end
+            set(obj.calibrationCard.deviceListBox, 'String', names);
+            set(obj.calibrationCard.deviceListBox, 'Values', values);
+        end
+        
+        function onSelectedDevice(obj, ~, ~)            
+            [device, gain] = obj.getSelectedDevice();
+            obj.selectDevice(device, gain);
+        end
+        
+        function selectDevice(obj, device, gain)
+            obj.setSelectedDevice(device, gain);
+            obj.populateDetailsForDevice(device, gain);
+        end
+        
+        function selectNextDevice(obj)
+            old = get(obj.calibrationCard.deviceListBox, 'Value');
+            old = old{1};
+            
+            values = get(obj.calibrationCard.deviceListBox, 'Values');
+            i = find(cellfun(@(v)isequal(v, old), values), 1) + 1;
+            new = values{mod(i - 1, numel(values)) + 1};
+            
+            obj.selectDevice(new.device, new.gain);
+        end
+        
+        function setSelectedDevice(obj, device, gain)
+            v = struct('device', device, 'gain', gain);
+            set(obj.calibrationCard.deviceListBox, 'Value', v);
+        end
+        
+        function [device, gain] = getSelectedDevice(obj)
+            v = get(obj.calibrationCard.deviceListBox, 'Value');
+            v = v{1};
+            device = v.device;
+            gain = v.gain;
+        end
+        
+        function populateDetailsForDevice(obj, device, gain)
+            
+        end
+        
         function onSelectedUse(obj, ~, ~)
-            disp('onSelectedUse');
+            [device, gain] = obj.getSelectedDevice();
+            
+            value = rand();
+            if obj.calibrations.isKey(device.name)
+                m = obj.calibrations(device.name);
+            else
+                m = containers.Map();
+            end
+            m(gain) = value;
+            obj.calibrations(device.name) = m;
+            
+            obj.setDeviceCalibrated(device, gain);
+            obj.selectNextDevice();
+            obj.updateStateOfControls();
+        end
+        
+        function setDeviceCalibrated(obj, device, gain)
+            names = get(obj.calibrationCard.deviceListBox, 'String');
+            values = get(obj.calibrationCard.deviceListBox, 'Values');
+            
+            i = cellfun(@(v)isequal(v, struct('device', device, 'gain', gain)), values);
+            n = ['<html><font color="green"><b>' device.name];
+            if ~strcmp(gain, 'none')
+                n = [n ' - ' gain];
+            end
+            names{i} = n;
+            
+            set(obj.calibrationCard.deviceListBox, 'String', names);
+            set(obj.calibrationCard.deviceListBox, 'Values', values);
+        end
+        
+        function tf = isDeviceCalibrated(obj, device, gain)
+            names = get(obj.calibrationCard.deviceListBox, 'String');
+            values = get(obj.calibrationCard.deviceListBox, 'Values');
+            
+            i = cellfun(@(v)isequal(v, struct('device', device, 'gain', gain)), values);
+            tf = strncmpi(names{i}, '<html><font color="green"><b>', 29);
         end
         
         function onSelectedLedOn(obj, ~, ~)
-            disp('onSelectedLedOn');
+            obj.updateStateOfControls();
         end
         
         function onSelectedSubmit(obj, ~, ~)
-            disp('onSelectedSubmit');
+            [device, gain] = obj.getSelectedDevice();
+            
+            value = rand();
+            if obj.calibrations.isKey(device.name)
+                m = obj.calibrations(device.name);
+            else
+                m = containers.Map();
+            end
+            m(gain) = value;
+            obj.calibrations(device.name) = m;
+            
+            obj.setDeviceCalibrated(device, gain);
+            obj.selectNextDevice();
+            obj.updateStateOfControls();
         end
         
         function onSelectedBack(obj, ~, ~)
@@ -231,13 +395,42 @@ classdef DeviceCalibrator < symphonyui.ui.Module
         end
         
         function onSelectedCancel(obj, ~, ~)
-            obj.stop();
+            obj.close();
         end
         
         function updateStateOfControls(obj)
             import appbox.*;
             
-            set(obj.backButton, 'Enable', onOff(get(obj.wizardCardPanel, 'Selection') > 1));
+            isLedOn = get(obj.calibrationCard.ledOnButton, 'Value');
+            isLastCard = get(obj.wizardCardPanel, 'Selection') >= numel(get(obj.wizardCardPanel, 'Children'));
+            allCalibrated = all(cellfun(@(s)obj.isDeviceCalibrated(s.device, s.gain), get(obj.calibrationCard.deviceListBox, 'Values')));
+            
+            set(obj.calibrationCard.calibrationVoltageField, 'Enable', onOff(~isLedOn));
+            set(obj.calibrationCard.powerReadingField, 'Enable', onOff(isLedOn));
+            set(obj.calibrationCard.spotDiameterField, 'Enable', onOff(isLedOn));
+            set(obj.calibrationCard.submitButton, 'Enable', onOff(isLedOn));
+            set(obj.backButton, 'Enable', onOff(isLastCard));
+            set(obj.nextButton, 'Enable', onOff(~isLastCard || allCalibrated));
+            if isLastCard
+                set(obj.nextButton, 'String', 'Finish');
+            else
+                set(obj.nextButton, 'String', 'Next >');
+            end
+        end
+        
+        function close(obj)
+            shouldClose = true;
+            if ~isempty(obj.calibrations)
+                result = obj.view.showMessage( ...
+                    ['You have calibrated some devices. You will lose these values if you close the calibrator. ' ...
+                    'Are you sure you want to close?'], 'Close', ...
+                    'button1', 'Cancel', ...
+                    'button2', 'Close');
+                shouldClose = strcmp(result, 'Close');
+            end
+            if shouldClose
+                obj.stop();
+            end
         end
         
     end
