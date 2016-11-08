5615a4caf5effb069be34752ad83404d97ed3fdf
Mark Cafaro <mark.cafaro@gmail.com>
2016-11-08 14:22:02 -0800

    Added below/above designations to stage rig descriptions

---
+++ src/main/matlab/+edu/+washington/+riekelab/+rigs/ConfocalWithLightCrafterAbove.m
@@ -0,0 +1,23 @@
+classdef ConfocalWithLightCrafterAbove < edu.washington.riekelab.rigs.Confocal
+    
+    methods
+        
+        function obj = ConfocalWithLightCrafterAbove()
+            import symphonyui.builtin.devices.*;
+            import edu.washington.*;
+            
+            daq = obj.daqController;
+            
+            lightCrafter = riekelab.devices.LightCrafterDevice('micronsPerPixel', 1.3);
+            lightCrafter.bindStream(daq.getStream('doport1'));
+            daq.getStream('doport1').setBitPosition(lightCrafter, 15);
+            obj.addDevice(lightCrafter);
+            
+            frameMonitor = UnitConvertingDevice('Frame Monitor', 'V').bindStream(daq.getStream('ai7'));
+            obj.addDevice(frameMonitor);
+        end
+        
+    end
+    
+end
+
---
+++ src/main/matlab/+edu/+washington/+riekelab/+rigs/ConfocalWithMicrodisplayBelow.m
@@ -0,0 +1,29 @@
+classdef ConfocalWithMicrodisplayBelow < edu.washington.riekelab.rigs.Confocal
+    
+    methods
+        
+        function obj = ConfocalWithMicrodisplayBelow()
+            import symphonyui.builtin.devices.*;
+            import edu.washington.*;
+            
+            daq = obj.daqController;
+            
+            ramps = containers.Map();
+            ramps('minimum') = linspace(0, 65535, 256);
+            ramps('low')     = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'confocal', 'microdisplay_below_low_gamma_ramp.txt'));
+            ramps('medium')  = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'confocal', 'microdisplay_below_medium_gamma_ramp.txt'));
+            ramps('high')    = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'confocal', 'microdisplay_below_high_gamma_ramp.txt'));
+            ramps('maximum') = linspace(0, 65535, 256);
+            microdisplay = riekelab.devices.MicrodisplayDevice('gammaRamps', ramps, 'micronsPerPixel', 1.2, 'comPort', 'COM3');
+            microdisplay.bindStream(daq.getStream('doport1'));
+            daq.getStream('doport1').setBitPosition(microdisplay, 15);
+            obj.addDevice(microdisplay);
+            
+            frameMonitor = UnitConvertingDevice('Frame Monitor', 'V').bindStream(daq.getStream('ai7'));
+            obj.addDevice(frameMonitor);
+        end
+        
+    end
+    
+end
+
