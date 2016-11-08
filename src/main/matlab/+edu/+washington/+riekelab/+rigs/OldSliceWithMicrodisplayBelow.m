6e043a8469b04185eaf80879b46295a2a815ddff
Mark Cafaro <mark.cafaro@gmail.com>
2016-11-08 14:22:02 -0800

    Added below/above designations to stage rig descriptions

---
+++ src/main/matlab/+edu/+washington/+riekelab/+rigs/OldSliceWithMicrodisplayBelow.m
@@ -0,0 +1,34 @@
+classdef OldSliceWithMicrodisplayBelow < edu.washington.riekelab.rigs.OldSlice
+    
+    methods
+        
+        function obj = OldSliceWithMicrodisplayBelow()
+            import symphonyui.builtin.devices.*;
+            import edu.washington.*;
+            
+            daq = obj.daqController;
+            
+            ramps = containers.Map();
+            ramps('minimum') = linspace(0, 65535, 256);
+            ramps('low')     = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'old_slice', 'microdisplay_below_low_gamma_ramp.txt'));
+            ramps('medium')  = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'old_slice', 'microdisplay_below_medium_gamma_ramp.txt'));
+            ramps('high')    = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'old_slice', 'microdisplay_below_high_gamma_ramp.txt'));
+            ramps('maximum') = linspace(0, 65535, 256);
+            microdisplay = riekelab.devices.MicrodisplayDevice('gammaRamps', ramps, 'micronsPerPixel', 1.2, 'comPort', 'COM3');
+            microdisplay.bindStream(daq.getStream('doport1'));
+            daq.getStream('doport1').setBitPosition(microdisplay, 15);
+            microdisplay.addResource('fluxFactorPaths', containers.Map( ...
+                {'low', 'medium', 'high'}, { ...
+                riekelab.Package.getCalibrationResource('rigs', 'old_slice', 'microdisplay_below_low_flux_factors.txt'), ...
+                riekelab.Package.getCalibrationResource('rigs', 'old_slice', 'microdisplay_below_medium_flux_factors.txt'), ...
+                riekelab.Package.getCalibrationResource('rigs', 'old_slice', 'microdisplay_below_high_flux_factors.txt')}));
+            obj.addDevice(microdisplay);
+            
+            frameMonitor = UnitConvertingDevice('Frame Monitor', 'V').bindStream(daq.getStream('ai7'));
+            obj.addDevice(frameMonitor);
+        end
+        
+    end
+    
+end
+
---
+++ src/main/matlab/+edu/+washington/+riekelab/+rigs/TwoPhotonWithMicrodisplayBelow.m
@@ -0,0 +1,29 @@
+classdef TwoPhotonWithMicrodisplayBelow < edu.washington.riekelab.rigs.TwoPhoton
+    
+    methods
+        
+        function obj = TwoPhotonWithMicrodisplayBelow()
+            import symphonyui.builtin.devices.*;
+            import edu.washington.*;
+            
+            daq = obj.daqController;
+            
+            ramps = containers.Map();
+            ramps('minimum') = linspace(0, 65535, 256);
+            ramps('low')     = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'two_photon', 'microdisplay_below_low_gamma_ramp.txt'));
+            ramps('medium')  = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'two_photon', 'microdisplay_below_medium_gamma_ramp.txt'));
+            ramps('high')    = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'two_photon', 'microdisplay_below_high_gamma_ramp.txt'));
+            ramps('maximum') = linspace(0, 65535, 256);
+            microdisplay = riekelab.devices.MicrodisplayDevice('gammaRamps', ramps, 'micronsPerPixel', 1.8, 'comPort', 'COM3');
+            microdisplay.bindStream(daq.getStream('doport1'));
+            daq.getStream('doport1').setBitPosition(microdisplay, 15);
+            obj.addDevice(microdisplay);
+            
+            frameMonitor = UnitConvertingDevice('Frame Monitor', 'V').bindStream(daq.getStream('ai7'));
+            obj.addDevice(frameMonitor);
+        end
+        
+    end
+    
+end
+
